93f7b7f784aa96f833be626b1db17efbab026887
myhiy <myhiyyyy@gmail.com>
2023-07-16 19:06:18 +0100

    IDK

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import cloudflare from "@astrojs/cloudflare";
-import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify/functions";
 const site = "https://bettervlr.com";
 
 // https://astro.build/config
@@ -28,5 +28,5 @@ export default defineConfig({
     ],
     output: "server",
     //adapter: cloudflare(),
-    adapter: vercel(),
+    adapter: netlify(),
 });
